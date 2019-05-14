2a3ee906aaceac6a982ba74f5aad4a344b235d8b
Rahul Raj <omrahulrajcse@gmail.com>
2019-05-14 23:39:51 +0530

    [update] eInfluencer App for GMO marketing hackathon

--- app/src/main/java/com/expedia/einfluencer/ItineraryDashBoardActivity.java
+++ app/src/main/java/com/expedia/einfluencer/ItineraryDashBoardActivity.java
@@ -1,12 +1,47 @@
 package com.expedia.einfluencer;
 
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ItineraryDashBoardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerary_dashboard);
+
+        Button itin1Button = (Button) findViewById(R.id.button);
+        itin1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new ItinFragmentOne());
+            }
+        });
+
+        Button itin2Button = (Button) findViewById(R.id.button2);
+        itin2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new ItinFragmentTwo());
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // create a FragmentManager
+        FragmentManager fm = getSupportFragmentManager();
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        // replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit(); // save the changes
     }
 }
+
+
