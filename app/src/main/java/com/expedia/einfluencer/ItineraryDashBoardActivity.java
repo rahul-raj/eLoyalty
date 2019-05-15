1138e2d7807ad60703feaadd987e387fecfc63e9
Rahul Raj <omrahulrajcse@gmail.com>
2019-05-16 01:10:06 +0530

    [update] eInfluencer App for GMO marketing hackathon

--- app/src/main/java/com/expedia/einfluencer/ItineraryDashBoardActivity.java
+++ app/src/main/java/com/expedia/einfluencer/ItineraryDashBoardActivity.java
@@ -1,14 +1,26 @@
 package com.expedia.einfluencer;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ItineraryDashBoardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerary_dashboard);
+
+        Button itin1Button = (Button) findViewById(R.id.button3);
+        itin1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ItineraryDashBoardActivity.this,SocialInfluencerSignUpActivity.class);
+                startActivity(intent);
+            }
+        });
 
        /* Button itin1Button = (Button) findViewById(R.id.button);
         itin1Button.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/expedia/einfluencer/SocialInfluencerSignUpActivity.java
@@ -0,0 +1,12 @@
+package com.expedia.einfluencer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SocialInfluencerSignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_influencer_signup);
+    }
+}
